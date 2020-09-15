ae9c4b7d16035688af7480323279f41abf206e7f
devies2 <68522356+devies2@users.noreply.github.com>
2020-09-15 17:03:14 +0900

    애니메이션 수정

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-$(function($){
-        $('.tabTit a').on('click',function(e){
-            e.preventDefault();
-            console.log($(this).index());
-            var ind = $(this).index();
-            $('.tabCont').eq(ind).siblings().removeClass('on');
-            $('.tabCont').eq(ind).addClass('on');
-        })
-})(jQuery)
+// $(function($){
+//         $('.tabTit a').on('click',function(e){
+//             e.preventDefault();
+//             console.log($(this).index());
+//             var ind = $(this).index();
+//             console.log(ind);
+//             $('.tabCont').eq(ind).siblings().removeClass('on');
+//             $('.tabCont').eq(ind).addClass('on');
+//         })
+// })(jQuery)
