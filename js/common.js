2fa8044c6be678a8890cb34ca1e59a03a14e72bc
devies2 <68522356+devies2@users.noreply.github.com>
2020-09-21 09:33:38 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -17,7 +17,12 @@
         $('#container').remove();
         $('#containerBox').load(url);
     })
-
+    $('.more a').on('click',function(e){
+        e.preventDefault();
+        var url =$(this).attr('href');
+        $('#container').remove();
+        $('#containerBox').load(url);
+    })
 
     // $('.logo_nav h1 a').on('click',function(e){
     //     e.preventDefault();
