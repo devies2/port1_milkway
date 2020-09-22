d7f180e014be04d90f586c080b7c6bc2172be2d4
devies2 <68522356+devies2@users.noreply.github.com>
2020-09-22 17:27:40 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -38,6 +38,7 @@
         // 스크롤 했을 때 진행프로젝트 영역
         var sct = $(this).scrollTop();
         var part3Box = $('.part3').offset().top - $(this).height() / 2
+        var part5Box = $('.part5').offset().top - $(this).height() / 2
         // var notNear = $('.part3').offset().top - $(this).height()/1
         if (sct >= part3Box) {
             $('.box-img').addClass('on')
@@ -48,5 +49,12 @@
             $('.box-text-wrap').removeClass('on')
             // console.log('여기까지2')
         }
+        if (sct >= part5Box) {
+            $('.part5-box').addClass('on')
+        } else if (sct <= part5Box-450) {
+            $('.part5-box').removeClass('on')
+        }
+
+
     })
 })(jQuery)
