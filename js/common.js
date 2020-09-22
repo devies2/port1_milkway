218916d85d87557c4f7486de06c8a2038b44e5e1
devies2 <68522356+devies2@users.noreply.github.com>
2020-09-22 16:55:47 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -14,8 +14,10 @@
     $('.depth1 a').on('click', function(e){
         e.preventDefault();
         var url = $(this).attr('href');
+        console.log(url);
         $('#container').remove();
         $('#containerBox').load(url);
+        return false;
     })
     $('.more a').on('click',function(e){
         e.preventDefault();
@@ -24,10 +26,27 @@
         $('#containerBox').load(url);
     })
 
+
     // $('.logo_nav h1 a').on('click',function(e){
     //     e.preventDefault();
     //     var url = $(this).attr('href');
     //     $('#container').remove();
     //     $('#containerBox').load(url)
     // })
+
+    $(window).scroll(function () {
+        // 스크롤 했을 때 진행프로젝트 영역
+        var sct = $(this).scrollTop();
+        var part3Box = $('.part3').offset().top - $(this).height() / 2
+        // var notNear = $('.part3').offset().top - $(this).height()/1
+        if (sct >= part3Box) {
+            $('.box-img').addClass('on')
+            $('.box-text-wrap').addClass('on')
+            // console.log('여기까지')
+        } else if (sct <= part3Box-450) {
+            $('.box-img').removeClass('on')
+            $('.box-text-wrap').removeClass('on')
+            // console.log('여기까지2')
+        }
+    })
 })(jQuery)
--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
-// $(function($){
-//         $('.tabTit a').on('click',function(e){
-//             e.preventDefault();
-//             console.log($(this).index());
-//             var ind = $(this).index();
-//             console.log(ind);
-//             $('.tabCont').eq(ind).siblings().removeClass('on');
-//             $('.tabCont').eq(ind).addClass('on');
-//         })
-// })(jQuery)
+$(function($){
+        // $('.tabTit a').on('click',function(e){
+        //     e.preventDefault();
+        //     console.log($(this).index());
+        //     var ind = $(this).index();
+        //     console.log(ind);
+        //     $('.tabCont').eq(ind).siblings().removeClass('on');
+        //     $('.tabCont').eq(ind).addClass('on');
+        // })
+        $('.depth1 >li').on('mouseenter',function(){
+            console.log('실행')
+            $(this).find('.depth2').slideDown(300);
+            $(this).find('.detail').slideDown(300);
+        })
+
+        $('.depth1 >li').on('mouseleave',function(){
+            console.log('실행')
+            $(this).find('.depth2').hide(700);
+            $(this).find('.detail').hide(700);
+        })
+
+})(jQuery)
